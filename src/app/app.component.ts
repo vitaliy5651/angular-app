01f8c95d0fbf10bd58bb2bf1baf96cc3d3b79755
группы стационара <st@itstep.lan>
2021-03-24 21:51:02 +0300

    21:53 24.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {  DataService } from './services/data/data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
- 
+ public slideritems: Array<{name: string, price: number}> = [
+   {
+     name: 'T-shirt',
+     price: 30,
+   },
+   {
+    name: 'Test',
+    price: 50,
+   },
+   {
+    name: 'Hat',
+    price: 20,
+   },
+   {
+    name: 'Panto',
+    price: 40,
+   },
+   {
+    name: 'Desk',
+    price: 70,
+   },
+ ]
 
   constructor() {
-    function myFunction() {
-      // Get the checkbox
-      var checkBox = document.getElementById("myCheck");
-    }
   }
  
 
